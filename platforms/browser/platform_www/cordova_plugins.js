a7d7a772213d9775a2b734044e7465d4149cef29
Devashish Datt Mamgain <devashish@applozic.com>
2017-03-16 02:44:46 +0530

    Revert "Phonegap forward option"
    This reverts commit 9e26dfd7c164be3ba4f5cbcd8d0bd947a6d3d08a.

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -7,55 +7,13 @@ module.exports = [
         "clobbers": [
             "PushNotification"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
-        "id": "cordova-plugin-camera.Camera",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "Camera"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-        "id": "cordova-plugin-camera.CameraPopoverOptions",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "CameraPopoverOptions"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/Camera.js",
-        "id": "cordova-plugin-camera.camera",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "navigator.camera"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
-        "id": "cordova-plugin-camera.CameraProxy",
-        "pluginId": "cordova-plugin-camera",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-webrtc/dist/webrtc.js",
-        "id": "cordova-plugin-webrtc.WebRTC",
-        "pluginId": "cordova-plugin-webrtc",
-        "clobbers": [
-            "webrtc"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "phonegap-plugin-push": "1.6.3",
-    "cordova.plugins.diagnostic": "3.4.2",
-    "cordova-plugin-camera": "2.1.1",
-    "cordova-plugin-webrtc": "0.1.1",
-    "cordova-opentok-android-permissions": "1.0.0"
+    "phonegap-plugin-push": "1.6.3"
 }
 // BOTTOM OF METADATA
 });
